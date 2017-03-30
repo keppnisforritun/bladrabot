f6e7d7c08d6e34d81e9e689f61d8e16e301dc1ea
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2017-03-30 22:07:12 +0000

    A simple template to work with

---
+++ bladra.py
@@ -0,0 +1,23 @@
+import discord
+import secret
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!halló'):
+        msg = 'Halló {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(secret.token)
